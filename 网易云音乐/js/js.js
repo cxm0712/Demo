e24ea8729abf49a8c2b42e1d1d56d6d1e29b9181
cxm我老婆 <50448135+cxm0712@users.noreply.github.com>
2019-06-11 18:16:35 +0800

    Add files via upload

--- 网易云音乐/js/js.js
+++ 网易云音乐/js/js.js
@@ -9,12 +9,12 @@ $.ajax({
 			for(var i = 1; i < t.length; i++) {
 				$(".nav_one_ul").append($("<li><a href='#'>" + t[i] + "</a></li>"));
 			}
-			$(".nav_one_ul>li>a:lt(3)").click(function(){
+		/*	$(".nav_one_ul>li>a:lt(3)").click(function(){
 				alert("该项目由于开发者懒惰，并未开发~");
 			})
 			$(".nav_one_ul>li>a:eq(5)").click(function(){
 				alert("该项目由于开发者懒惰，并未开发~");
-			})
+			})*/
 			$(".nav_one_ul>li>a:eq(4)").attr("href","w_index.html");
 		})
 		/*nav_two*/
